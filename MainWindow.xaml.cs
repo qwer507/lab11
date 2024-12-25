6b711b92ae16d5cdb4f9ef7bafee7e012422a832
RedPen <RedPen@DESKTOP-S7MN8QE>
2024-12-25 19:55:55 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<MyUserControl> all_products = new List<MyUserControl>()
+            {
+                new MyUserControl{ Name_prod = "Иконка", Info = "Просто иконка", Manufacturer = "Icon inc", Price = 399, Path_to_img = "img\\prod1.png", Prod_on_stor = 17, Margin=new Thickness(10) },
+                new MyUserControl{ Name_prod = "Варежки", Info = "Синие и тёплые", Manufacturer = "Icon inc", Price = 1999, Path_to_img = "img\\prod2.jpg", Prod_on_stor = 23, Margin=new Thickness(10) },
+                new MyUserControl{ }
+            };
+        ObservableCollection<MyUserControl> need_products = new ObservableCollection<MyUserControl>();
+        List<string> manufacturers = new List<string>();
+        public MainWindow()
+        {
+            need_products = new ObservableCollection<MyUserControl>(all_products);
+            InitializeComponent();
+            Products.ItemsSource = need_products;
+            List<string> manufacturers = all_products.Select(e => e.Manufacturer).Distinct().ToList();
+            manufacturers.Insert(0, "Все производители");
+            combobox_manu.ItemsSource = manufacturers;
+        }
+
+        private void EditListProducts(object sender, SelectionChangedEventArgs e)
+        {
+            filtr_products();
+        }
+
+        private void textbox_name_and_info_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            filtr_products();
+        }
+
+        private void filtr_products()
+        {
+            string searchTerm = textbox_name_and_info.Text.ToLower();
+            string selectedManufacturer = (string)combobox_manu.SelectedItem;
+            var a = all_products.Where(p => (string.IsNullOrEmpty(searchTerm) || p.Name_prod.ToLower().Contains(searchTerm) || p.Info.ToLower().Contains(searchTerm)) && ((string.IsNullOrEmpty(selectedManufacturer) || p.Manufacturer == selectedManufacturer || selectedManufacturer == "Все производители")));
+            if (combobox_price.SelectedIndex == 1)
+                a = a.OrderBy(e => e.Price);
+            need_products.Clear();
+            foreach (var i in a)
+                need_products.Add(i);
+        }
+    }
+}
---
+++ MyUserControl.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MyUserControl.xaml
+    /// </summary>
+    public partial class MyUserControl : UserControl
+    {
+        // На складе
+        public int Prod_on_stor
+        {
+            get { return (int)GetValue(Prod_on_storProperty); }
+            set { SetValue(Prod_on_storProperty, value); }
+        }
+
+        public static readonly DependencyProperty Prod_on_storProperty =
+            DependencyProperty.Register("Prod_on_stor", typeof(int), typeof(MyUserControl), new PropertyMetadata(0));
+
+        // Название
+        public string Name_prod
+        {
+            get { return (string)GetValue(Name_prodProperty); }
+            set { SetValue(Name_prodProperty, value); }
+        }
+
+        public static readonly DependencyProperty Name_prodProperty =
+            DependencyProperty.Register("Name_prod", typeof(string), typeof(MyUserControl), new PropertyMetadata("None"));
+
+        // ИНформация
+        public string Info
+        {
+            get { return (string)GetValue(InfoProperty); }
+            set { SetValue(InfoProperty, value); }
+        }
+
+        public static readonly DependencyProperty InfoProperty =
+            DependencyProperty.Register("Info", typeof(string), typeof(MyUserControl), new PropertyMetadata("None"));
+
+        // Производитель
+        public string Manufacturer
+        {
+            get { return (string)GetValue(ManufacturerProperty); }
+            set { SetValue(ManufacturerProperty, value); }
+        }
+
+        public static readonly DependencyProperty ManufacturerProperty =
+            DependencyProperty.Register("Manufacturer", typeof(string), typeof(MyUserControl), new PropertyMetadata("None"));
+
+        // Цена
+        public int Price
+        {
+            get { return (int)GetValue(PriceProperty); }
+            set { SetValue(PriceProperty, value); }
+        }
+
+        public static readonly DependencyProperty PriceProperty =
+            DependencyProperty.Register("Price", typeof(int), typeof(MyUserControl), new PropertyMetadata(0));
+
+        // Путь к изображению
+        public string Path_to_img
+        {
+            get { return (string)GetValue(Path_to_imgProperty); }
+            set { SetValue(Path_to_imgProperty, value); }
+        }
+
+        public static readonly DependencyProperty Path_to_imgProperty =
+            DependencyProperty.Register("Path_to_img", typeof(string), typeof(MyUserControl), new PropertyMetadata("img\\default.png"));
+
+        public MyUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
